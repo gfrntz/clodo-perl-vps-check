755d134d8cc5fb28136f949cb5bca2a7cc768f8b
Cepnoy <chainwolf@gmail.com>
2012-04-16 04:12:22 +0400

    set options

--- main.pl
+++ main.pl
@@ -15,3 +15,74 @@ use HTTP::Headers;
 use HTTP::Request;
 
 
+use vars qw(
+	$np
+	$options
+	$usage
+	$extra
+	$version
+);
+
+$np = Nagios::Plugin->new( shortname => 'CLODO_MONIT' );
+
+	$options = Nagios::Plugin::Getopt->new(
+		usage	=> $usage,
+		extra	=> $extra,
+		version	=> $version,
+		url		=> 'https://github.com/Cepnoy/clodo-perl-vps-check',
+		blurb	=> 'Check clodo corp client\'s vps',
+	);	
+	
+	$options->arg(
+		spec	=> 'api=s',
+		help	=> 'set api addres',
+		required => 1,
+	);
+	
+	$options->arg(
+		spec	=> 'testapi',
+		help	=> 'test api connect',
+		required => 0,
+	);
+
+	$options->arg(
+		spec	=> 'id=s',
+		help	=>	'set vps id',
+		required => 1,
+	);
+	
+	$options->arg(
+		spec	=> 'mcc=i',
+		help	=> 'set max cpu critical value',
+		required => 0,
+	);
+	
+	$options->arg(
+		spec	=> 'mm=i',
+		help	=> 'set max memory critical value in KB',
+		required => 0,
+	);
+	
+	$options->arg(
+		spec	=> 'mii=i',
+		help	=> 'set max interface input traffic',
+		required => 0,
+	);
+	
+	$options->arg(
+		spec	=> 'mio=i',
+		help	=> 'set max output interface traffic',
+		required => 0,
+	);
+
+	$options->arg(
+		spec	=> 'mhu=i',
+		help	=> 'set max hdd usage',
+		required => 0,
+	);
+	
+	$options->arg(
+		spec	=> 'checkbalance',
+		help	=> 'check negative balance',
+		required => 0,
+	);
